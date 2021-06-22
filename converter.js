9b3cd6278328b6add9b1f08e6c47032f2fb10fc2
Mikulew <mikulew@gmail.com>
2021-06-22 21:30:21 +0200

    Add convert script and output file

--- converter.js
+++ converter.js
@@ -6,7 +6,7 @@ dataForge.readFileSync("csv/Transactions-02.csv")     // Read input file.
   .parseInts("Column2", "Column3")                    // Parse from strings to integers.
   .parseDates("Column4")                              // Parse from strings to date objects.
   .dropSeries("Column5")                              // Don't want column 5.
-  // .where(row => predicate(row))                       // Filter out rows that you don't want.
-  // .select(row => transform(row))                      // Apply transformation to each row.
-  .asJSON()                                            // Write out data file in CSV (or other) format.
+  // .where(row => predicate(row))                    // Filter out rows that you don't want.
+  // .select(row => transform(row))                   // Apply transformation to each row.
+  .asJSON()                                           // Write out data file in CSV (or other) format.
   .writeFileSync("output.json");
